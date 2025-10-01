7e3cd388a8ac875635459c5e20c557fe1d7e5645
xtsusaku <nanaxtsusaku@gmail.com>
2025-10-02 03:34:42 +0700

    Make Script Independence!

--- NanaTweaksUtilities.user.js
+++ NanaTweaksUtilities.user.js
@@ -6,7 +6,6 @@
 // @author       You
 // @match        http*://*.animemusicquiz.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
-// @require      https://github.com/xtsusaku/amq-scripts/raw/main/NanaTweaksKeybinds.user.js
 // @downloadURL  https://github.com/xtsusaku/amq-scripts/raw/main/NanaTweaksUtilities.user.js
 // @updateURL    https://github.com/xtsusaku/amq-scripts/raw/main/NanaTweaksUtilities.user.js
 // ==/UserScript==
