f52d597b0883cdd13fee128e5d5cba9c0174966e
xtsusaku <20338766+xtsusaku@users.noreply.github.com>
2026-08-29 17:33:19 +0700

    Add NanaTweaks Chat Commands user script

---
+++ NanaTweaksChatCommands.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         NanaTweaks Chat Commands
+// @namespace    https://xtsusaku.net/
+// @version      0.0.5
+// @description  AMQ Tweaks (request made)
+// @author       You
+// @match        http*://*.animemusicquiz.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
+// @downloadURL  https://github.com/xtsusaku/amq-scripts/raw/main/NanaTweaksKeybinds.user.js
+// @updateURL    https://github.com/xtsusaku/amq-scripts/raw/main/NanaTweaksKeybinds.user.js
+// ==/UserScript==
+
+"use strict";
+
+if (typeof Listener === "undefined") return;
+
+let NanaTweaksNanaTweaksChatCommandsLoadInterval = setInterval(() => {
+    if ($("#loadingScreen").hasClass("hidden")) {
+        clearInterval(NanaTweaksNanaTweaksChatCommandsLoadInterval);
+        NanaTweaksChatCommands.setup();
+        document.NanaTweaksChatCommands = NanaTweaksChatCommands;
+    }
+}, 500);
+
+class NanaTweaksChatCommands {
+    // Example - { name: "potatoes", desc:"Potatoes GameMode", cb: (subs:string[]) => {} }
+    static commandList = [];
+
+    static setup() {
+        this.commandList = this.commandList || [];
+
+        document.querySelector("#gcInput").addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                const cmdSplit = event.target.value.split(" ");
+                const cmd = cmdSplit.shift().replace(/^\//g, "").toLowerCase();
+                for (const { name, cb } of this.commandList) {
+                    if (cmd === name) {
+                        cb(cmdSplit);
+                    }
+                }
+                if (this.commandList.find((c) => c.name === cmd)) {
+                    event.preventDefault();
+                    document.querySelector("#gcInput").value = "";
+                }
+            }
+        });
+    }
+
+    static addCommand(name, desc, cb) {
+        if (this.commandList.find((a) => a.name === name))
+            array.splice(
+                this.commandList.findIndex((a) => a.name === name),
+                1,
+            );
+        this.commandList.push({ name, desc, cb });
+    }
+
+    static sendChat(msg, teamMessage = false) {
+        socket.sendCommand({
+            type: "lobby",
+            command: "game chat message",
+            data: {
+                msg,
+                teamMessage,
+            },
+        });
+    }
+
+    static sendMessage(target, message, isOpenchat = false) {
+        if(isOpenchat) socialTab.chatBar.getChat(target)
+        socket.sendCommand({
+            type: "social",
+            command: "chat message",
+            data: {
+                target,
+                message
+            },
+        });
+        if(isOpenchat) socialTab.chatBar.getChat(target).writeMessage(selfName, message, {customEmojis: [], emotes:[], shotCodes: []})
+    }
+}
